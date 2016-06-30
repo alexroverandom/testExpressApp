c27f94d592833b001bd1a115c7b6dbbdb8b92100
alexroverandom <alexroverandom@gmail.com>
2016-07-01 00:53:07 +0300

    create small index app

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,3 +1,21 @@
 (function(){
 	console.log("We are here!");
+
+	var ui = {
+		welcome: $(".jWelcome"),
+		getText: $(".jGetText")
+	};
+
+	function bindEvents() {
+		ui.getText.on("click", function() {
+			$.get("/api/getwelcome/").done(function(data) {
+				ui.welcome.text(data);
+			}).fail(function(res) {
+				console.log("Error");
+			});
+		});
+	}
+
+	bindEvents();
+	
 })();
--- server.js
+++ server.js
@@ -4,10 +4,6 @@ var path = require('path');
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
-  res.send("Hello Evgeniya");
-});
-
 app.get("/books/", function (req, res) {
   var books = [];
   res.send("Books");
@@ -17,9 +13,13 @@ app.get('/authors/', function (req, res) {
   res.send("List of authors here");
 });
 
-app.get('*', function (req, res) {
+app.get('/api/getwelcome/', function (req, res) {
+  res.send("Evgeniya, I love you");
+});
+
+app.get("*", function (req, res) {
 	console.log(__dirname);
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+  res.sendFile(path.join(__dirname + "/public"));
 });
 
 app.listen(8848, function () {
