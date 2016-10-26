a18dcac270915098e8c2ac19454a077151dbaf0b
alexroverandom <alexroverandom@gmail.com>
2016-10-26 14:39:59 +0300

    some changes for deploy

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + "/public"));
 });
 
-app.listen(8848, function () {
-  console.log("Example app listening on port 8848!");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8848
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
